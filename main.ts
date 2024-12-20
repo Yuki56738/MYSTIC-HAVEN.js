c40aa15427cfcd2bdd9cbc83a689ca68d8119fc2
Yuki Ito <github@risaton.net>
2024-12-21 08:50:29 +0900

    Add 'setchannel' command to configure logging channel
    Introduced a new slash command 'setchannel' to allow users to set a logging channel via channel ID. Updated the command registration logic to target a specific guild and added error handling for invalid channel IDs. This enhancement improves configurability and flexibility for logging.

--- main.ts
+++ main.ts
@@ -12,11 +12,19 @@ let commands = [];
 const comandPing= new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Replies with Pong!');
+const commandSetChannel = new SlashCommandBuilder()
+    .setName('setchannel')
+    .setDescription('Sets the channel to log to.')
+    .addStringOption(option => option.setName('channel').setDescription('The channel to log to with channel ID.').setRequired(true));
 commands.push(comandPing.toJSON());
+commands.push(commandSetChannel.toJSON());
 client.on('ready', async e => {
     // console.log(`Logged in as ${client.user?.tag}!`);
     logger(`Logged in as: ${client.user.tag}`, false)
-    await e.application?.commands.set(commands);
+    // await e.application?.commands.set(commands);
+    await client.guilds.fetch(TEST_GUILD_ID).then(async guild => {
+        await guild.commands.set(commands);
+    })
 });
 
 client.on('interactionCreate', async interaction => {
@@ -30,6 +38,20 @@ client.on('interactionCreate', async interaction => {
             logger(`Error: ${e}`, true)
         }
         return
+    }
+    if (interaction.commandName === 'setchannel'){
+        logger(`Set channel command hit.`, false)
+        try{
+            const channel_id: string = interaction.options.getString('channel');
+            if (!channel_id) {
+                logger('Error: Channel ID is null or undefined.', true);
+                return;
+            }
+
+            logger(`Channel ID retrieved: ${channel_id}`, false);
+        }catch (e) {
+            logger(`Error: ${e}`, true)
+        }
     }
 })
 

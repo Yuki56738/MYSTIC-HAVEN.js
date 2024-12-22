d45d69f595e798f5d5504d0ae9b5e2b235599b07
Yuki Ito <github@risaton.net>
2024-12-22 14:44:02 +0900

    Remove commented-out code and improve formatting.
    Cleaned up unused and commented-out code to enhance readability and maintainability. Adjusted formatting for consistency across the file and clarified debug logging messages.

--- main.ts
+++ main.ts
@@ -1,5 +1,13 @@
 import {PrismaClient} from "@prisma/client";
-import {Client, IntentsBitField, SlashCommandBuilder, Events, TextChannel, Interaction} from 'discord.js';
+import {
+    Client,
+    IntentsBitField,
+    SlashCommandBuilder,
+    Events,
+    TextChannel,
+    Interaction,
+    PermissionOverwrites
+} from 'discord.js';
 import dotenv from "dotenv";
 import {ChannelType} from 'discord.js';
 
@@ -30,10 +38,6 @@ let commands = [];
 const commandPing = new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Replies with Pong!');
-// const commandSetChannel = new SlashCommandBuilder()
-//     .setName('setchannel')
-//     .setDescription('Sets the channel to log to.')
-//     .addStringOption(option => option.setName('channel').setDescription('The channel to log to with channel ID.').setRequired(true));
 const commandGetChannel = new SlashCommandBuilder()
     .setName('getchannel')
     .setDescription('Gets the channel to log to.');
@@ -52,7 +56,7 @@ commands.push(commandDebug.toJSON());
 client.on('ready', async () => {
     logger.info(`Logged in as: ${client.user?.tag}`)
     logger.info(`Connecting to following guilds:`)
-    client.guilds.cache.forEach( (guild) => {
+    client.guilds.cache.forEach((guild) => {
         logger.info(`- ${guild.name}`);
     })
     if (process.env.RAILWAY_ENVIRONMENT_NAME !== 'production') {
@@ -82,68 +86,23 @@ client.on(Events.InteractionCreate, async (interaction) => {
         }
         return
     }
-    // if (interaction.commandName === 'setchannel') {
-    //     logger.debug(`Set channel command hit.`)
-    //     try {
-    //         // @ts-ignore
-    //         const channel_id: string = interaction.options.getString('channel');
-    //         if (!channel_id) {
-    //             logger.error('Error: Channel ID is null or undefined.');
-    //             return;
-    //         }
-    //         logger.debug(`Channel ID retrieved: ${channel_id}`);
-    //         try {
-    //             logger.info(`Attempting to connect to database.`)
-    //             const guild = await client.guilds.fetch(interaction.guildId!)
-    //             const prisma = new PrismaClient()
-    //             // const allSettings = await prisma.settings.findMany()
-    //             // logger.debug(`All settings: ${allSettings}`)
-    //             await prisma.settings.upsert({
-    //                 where: {guild_id: BigInt(interaction.guildId!)},
-    //                 update: {},
-    //                 create: {
-    //                     guild_id: BigInt(interaction.guildId!),
-    //                     guild_name: guild.name!,
-    //                     set_user_id: BigInt(interaction.user.id!),
-    //                     channel_for_notify: channel_id!
-    //                 }
-    //             })
-    //             await prisma.$disconnect()
-    //         } catch (e) {
-    //             logger.error(`Error: ${e}`)
-    //         }
-    //     } catch (e) {
-    //         logger.error(`Error: ${e}`)
-    //     }
-    //     return
-    //
-    // }
     if (interaction.commandName === 'getchannel') {
         logger.debug(`Get channel command hit.`)
         try {
             await interaction.deferReply()
-            // const guild = await client.guilds.fetch(interaction.guildId!)
             logger.info('Attempting to connect to database.')
             const prisma = new PrismaClient()
-            // const allSettings = await prisma.settings.findMany()
-            // logger(`All settings: ${allSettings}`)
-            // const allSettings = await prisma.settings.findMany()
-            // logger.debug(`All settings: ${allSettings.map(setting => setting.channel_for_notify).join(', ')}`)
-            // await interaction.editReply(`募集版は、 ${allSettings.map(x => x.channel_for_notify)}`)
-            // allSettings['channel_for_notify']
             const guildId = BigInt(interaction.guildId!);
-            const setting = await prisma.settings.findUnique({ where: { guild_id: guildId } });
+            const setting = await prisma.settings.findUnique({where: {guild_id: guildId}});
 
             if (setting) {
                 const channelForNotify = setting.channel_for_notify;
                 // @ts-ignore
                 const channelForNotifyObj = await client.channels.fetch(channelForNotify) as TextChannel
                 await interaction.editReply(`募集版は、 ${channelForNotifyObj.name} (${channelForNotifyObj.id}).`)
-                // Use channelForNotify
-                console.log(`Channel for notify: ${channelForNotifyObj.name} (${channelForNotifyObj.id})`);
+                logger.debug(`Channel for notify: ${channelForNotifyObj.name} (${channelForNotifyObj.id})`);
             } else {
-                // Handle case where no setting is found for this guild
-                console.log(`No setting found for guild ID ${guildId}`);
+                logger.debug(`No setting found for guild ID ${guildId}`);
             }
             await prisma.$disconnect()
         } catch (e) {
@@ -170,8 +129,6 @@ client.on(Events.InteractionCreate, async (interaction) => {
             logger.debug(`Attempting to connect to database.`)
             const guild = await client.guilds.fetch(interaction.guildId!)
             const prisma = new PrismaClient()
-            // const allSettings = await prisma.settings.findMany()
-            // logger.debug(`All settings: ${allSettings}`)
             const db_setting = await prisma.settings.findFirst({
                 where: {guild_id: BigInt(interaction.guildId!)}
             })
@@ -186,7 +143,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
                         channel_for_notify: logChannelObj.id
                     }
                 })
-            }else{
+            } else {
                 await prisma.settings.create({
                     data: {
                         guild_id: BigInt(interaction.guildId!),
